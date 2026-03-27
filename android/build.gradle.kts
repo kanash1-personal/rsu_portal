638876c372a0851ed6529917c9a3f6483da9ef69
kanash1-personal <kim.panha7171@gmail.com>
2026-03-27 21:45:51 +0700

    trying to install plugins for firebase and authentication

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application") version "8.11.1" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -15,6 +20,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
